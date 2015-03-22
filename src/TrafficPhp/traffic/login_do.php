b21992ff0cdbf5fd25a388591bb62c668197a473
Tudor Cornea <tudor.cornea@gmail.com>
2015-03-22 19:41:13 +0200

    Correct login

--- src/TrafficPhp/traffic/login_do.php
+++ src/TrafficPhp/traffic/login_do.php
@@ -5,7 +5,7 @@ $db = SingletonDB::connect();
 
 session_start();
 
-$res = $db->query("SELECT * FROM users WHERE username='".$db->real_escape_string($_POST['username'])."' OR password='".sha1($db->real_escape_string($_POST['password']))."'");
+$res = $db->query("SELECT * FROM users WHERE username='".$db->real_escape_string($_POST['username'])."' AND password='".sha1($db->real_escape_string($_POST['password']))."'");
 
 if($res->num_rows != 0)
 {
